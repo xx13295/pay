f5b2d62844ba5df601306260216cc7b677e6bf54
王小明 <i@ojbk.plus>
2019-07-05 17:44:49 +0800

    支付宝支付

---
+++ src/main/java/plus/ojbk/pay/api/INotice.java
@@ -0,0 +1,13 @@
+package plus.ojbk.pay.api;
+
+import java.util.Map;
+
+public interface INotice {
+
+    /**
+     * 保存通知信息[订单信息]
+     *
+     * @param params
+     */
+    void saveNotice(Map<String, String> params);
+}
